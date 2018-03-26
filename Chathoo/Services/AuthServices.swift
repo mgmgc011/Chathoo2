fd214a68596a1ff4c06f24bef7c4c84c49c2638f
Chingoo <Chingoo@Mingus-MacBook-Pro.local>
2018-03-26 16:47:14 -0500

    AuthService Tweak

--- Chathoo/Services/AuthServices.swift
+++ Chathoo/Services/AuthServices.swift
@@ -76,7 +76,7 @@ class AuthService {
             
             if response.result.error == nil {
                 guard let data = response.data else { return }
-                let json = JSON(data: data)3
+                let json = JSON(data: data)
                 self.userEmail = json["user"].stringValue
                 self.authToken = json["token"].stringValue
                 
